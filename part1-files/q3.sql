ef6e8c5fd0fc834c02b69e6d795d0286d65f7bb8
Daher Anthony <daherant@teach.cs.toronto.edu>
2017-03-15 16:18:59 -0400

    p3 modification

--- part1-files/q3.sql
+++ part1-files/q3.sql
@@ -20,10 +20,12 @@ DROP VIEW IF EXISTS weightedMarks CASCADE;
 DROP VIEW IF EXISTS totalMarks CASCADE;
 DROP VIEW IF EXISTS soloWorkers CASCADE;
 DROP VIEW IF EXISTS soloAverages CASCADE;
+DROP VIEW IF EXISTS groupSize CASCADE;
+DROP VIEW IF EXISTS allAverages CASCADE;
 DROP VIEW IF EXISTS groupWorkers CASCADE;
 DROP VIEW IF EXISTS groupAverages CASCADE;
-DROP VIEW IF EXISTS iAsolo CASCADE;
-DROP VIEW IF EXISTS finalAssignments CASCADE;
+DROP VIEW IF EXISTS intermSoloAssignments CASCADE;
+DROP VIEW IF EXISTS interm CASCADE;
 
 CREATE VIEW weightedMarks AS SELECT assignment_id, group_id, grade.rubric_id, grade*weight as weightedGrade, out_of*weight as weightedOutOf 
 FROM RubricItem JOIN grade ON rubricitem.rubric_id = grade.rubric_id;
@@ -42,28 +44,28 @@ CREATE VIEW soloAverages AS SELECT assignment_id,AVG(percentage) as avg_solo, CO
 FROM totalmark JOIN soloWorkers on totalmark.group_id=soloworkers.group_id 
 Group BY assignment_id;
 
-CREATE VIEW groupSize AS SELECT group_id, COUNT(*) as group_count
-FROM membership
-Group by group_id;
+CREATE VIEW groupSize AS SELECT assignment_id, membership.group_id, COUNT(*) as group_count
+FROM assignmentGroup JOIN membership ON assignmentGroup.group_id = membership.group_id
+Group by assignment_id, membership.group_id;
 
-CREATE VIEW groupWorkers AS SELECT group_id, group_count
-FROM group_size
+CREATE VIEW groupWorkers AS SELECT assignment_id, group_id, group_count
+FROM groupSize
 WHERE group_count>1;
 
 CREATE VIEW allAverages AS SELECT assignment_id, AVG(group_count) as avg_size
 FROM groupSize
 Group By assignment_id; 
 
-CREATE VIEW groupAverages AS SELECT assignment_id, AVG(percentage) as avg_group, SUM(group_count) as group_size
+CREATE VIEW groupAverages AS SELECT totalMark.assignment_id, AVG(percentage) as avg_group, SUM(group_count) as group_size
 FROM totalmark JOIN groupworkers on totalmark.group_id=groupworkers.group_id 
-Group BY assignment_id;
+Group BY totalMark.assignment_id;
 
 
 CREATE VIEW intermSoloAssignments AS SELECT Assignment.assignment_id, Assignment.description, soloAverages.num_solo, soloAverages.avg_solo
 FROM soloAverages RIGHT OUTER JOIN Assignment on soloAverages.assignment_id = Assignment.assignment_id;
 
-CREATE VIEW interm AS SELECT assignment_id,description, num_solo, avg_solo, avg_group ,group_size, 
-FROM intermSoloAssignments JOIN groupAverages ON intermSoloAssignments.assignment_id=groupAverages.assignment_id;
+CREATE VIEW interm AS SELECT intermSoloAssignments.assignment_id, description, num_solo, avg_solo, avg_group ,group_size 
+FROM intermSoloAssignments LEFT OUTER JOIN groupAverages ON intermSoloAssignments.assignment_id=groupAverages.assignment_id;
 
 INSERT INTO q3 SELECT interm.assignment_id, interm.description, interm.num_solo, interm.avg_solo, interm.group_size, interm.avg_group, allAverages.avg_size
-FROM allAverages OUTER JOIN interm on interm.assignment_id = allAverages.assignment_id;
+FROM allAverages JOIN interm on interm.assignment_id = allAverages.assignment_id;
